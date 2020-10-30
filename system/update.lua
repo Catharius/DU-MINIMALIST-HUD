2042c77df5737cb6d8e0e64dfae5e8f80e23f2a0
Catharius <72415020+Catharius@users.noreply.github.com>
2020-10-30 21:58:33 +0100

    version 0.2

--- system/update.lua
+++ system/update.lua
@@ -1,7 +1,17 @@
 -- Copy and paste the code below at the end of update
--- Please note that if you already have a UI, you can add the desired ui part by finding system.setScreen and adding the html variable fuel_html or damage_html
--- (i do not guaranty it will work fine with an other ui)
+-- Please note that if you already have a UI, you can add the desired ui part by finding system.setScreen and adding the html variable fuel_html or damage_html along with damage_css
+-- For advanced lua coders, you can define your own css
+-- (Disclaimer : i do not guaranty it will work fine with an other ui)
+-- dmgrep:renderHTML() will return  all ui part {top_view_html,front_view_html,side_view_html,table_view_html}, example : dmgrep:renderHTML()[3] will return the side view part 
+
+-- minimalistic hud
 fuel_html=fm:renderHTML()
 damage_html=dmgrep:renderHTML()
-system.setScreen(fuel_html..damage_html)
-system.showScreen(1)
+damage_css=dmgrep:renderCSS()
+-- Show the selected view
+i = dmgrep:getActiveView() 
+txt_view = ""
+if MINHUD_show_txt_module == true then
+txt_view=damage_html[4]
+end
+system.setScreen(fuel_html..damage_css..damage_html[i]..txt_view)
